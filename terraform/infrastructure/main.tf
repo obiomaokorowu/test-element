6ee93fa9f13abda7af80be1d2d1d7d3394224a47
obiomaokorowu <austinobi983@gmail.com>
2025-01-01 12:42:59 +0100

    upload csv files to s3

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -78,8 +78,18 @@ resource "aws_iam_role_policy" "lambda_exec_policy" {
           "s3:PutObject"
         ],
         Resource = [
+          "arn:aws:s3:::austinobioma-datasets",
           "arn:aws:s3:::austinobioma-datasets/*"
         ]
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect   = "Allow",
+        Resource = "arn:aws:logs:*:*:*"
       }
     ]
   })
