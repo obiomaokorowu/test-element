e093e766e35dba74827de39849607cd902f78516
obiomaokorowu <austinobi983@gmail.com>
2025-01-01 11:44:17 +0100

    apigateway endpoint output

--- terraform/infrastructure/outputs.tf
+++ terraform/infrastructure/outputs.tf
@@ -12,3 +12,10 @@ output "lambda_execution_role" {
   value       = aws_iam_role.lambda_exec.name
   description = "IAM Role assigned to the Lambda function"
 }
+output "api_gateway_id" {
+  value = aws_apigatewayv2_api.http_api.id
+}
+
+output "api_gateway_endpoint" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
