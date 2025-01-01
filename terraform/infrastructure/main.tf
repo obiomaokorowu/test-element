eb13cbc3c5cb63c231000bb814120de4fe83caa4
obiomaokorowu <austinobi983@gmail.com>
2025-01-01 18:37:06 +0100

    use docker for pandas

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -151,3 +151,106 @@ resource "aws_s3_object" "csv_files" {
   }
 }
 
+# Create S3 bucket for Athena query results
+resource "aws_s3_bucket" "athena_results" {
+  bucket = "austinobioma-athena-results-bucket"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# Create Athena workgroup
+resource "aws_athena_workgroup" "default" {
+  name        = "austinobioma-workgroup"
+  state       = "ENABLED"
+  description = "Workgroup for querying the merged dataset"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena_results.bucket}/results/"
+    }
+  }
+}
+
+# IAM Policy for Athena Access
+resource "aws_iam_policy" "athena_access" {
+  name        = "athena-access-policy"
+  description = "Policy to allow Athena to query S3 and Lambda access"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:ListBucket"],
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.datasets.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.datasets.bucket}/*"
+        ]
+      },
+      {
+        Effect   = "Allow",
+        Action   = "athena:*",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the policy to Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_athena_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.athena_access.arn
+}
+# QuickSight Role
+resource "aws_iam_role" "quicksight_access_role" {
+  name = "QuickSightAccessRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "quicksight.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# QuickSight Policy for S3 Access
+resource "aws_iam_policy" "quicksight_s3_access" {
+  name = "QuickSightS3Access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:ListBucket"],
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.athena_results.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.athena_results.bucket}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attach Policy to QuickSight Role
+resource "aws_iam_role_policy_attachment" "quicksight_s3_access_attachment" {
+  role       = aws_iam_role.quicksight_access_role.name
+  policy_arn = aws_iam_policy.quicksight_s3_access.arn
+}
+
